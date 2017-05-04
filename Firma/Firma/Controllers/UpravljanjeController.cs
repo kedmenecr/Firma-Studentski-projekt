5f3d8a64468efbfd1e106fbdc19b8e9e629c4948
kedmenecr <kedmenec.roberto@gmail.com>
2017-05-04 19:59:30 +0200

    Dodana baza za artikle te prikaz istih
    ..

---
+++ Firma/Firma/Controllers/UpravljanjeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Firma.Models;
+using Firma.Models.PoslovnaLogika;
+
+namespace Firma.Controllers
+{
+    public class 
+        UpravljanjeController : Controller
+    {
+        private bazaContext artikliDb = new bazaContext();
+        
+
+        // GET: Upravljanje
+        public ActionResult Index()
+        {
+            var artikli = from z in artikliDb.artikli select z;
+            return View(artikli);
+        }
+
+        public ActionResult Unos()
+        {
+
+            return View();
+        }
+
+
+    }
+}
